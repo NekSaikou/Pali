2a2ef034b1f5596fb1fe0447457c9994ebae5dbf
Nek Saikou <NekSaikou@proton.me>
2024-05-03 17:39:43 +0700

    removed useless cast

--- src/search/LogTable.cpp
+++ src/search/LogTable.cpp
@@ -6,8 +6,8 @@
 std::array<double, 128> LogTable = {};
 
 void pali::initLogTable() {
-  for (int i = 0; i < 128; ++i)
-    const_cast<double &>(LogTable[i]) = std::log(i);
+  for (int i = 1; i < 128; ++i)
+    LogTable[i] = std::log(i);
 }
 
 double pali::ln(int i) { return LogTable[i]; }
